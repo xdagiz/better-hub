b0903e0b0a7fa543f7d758681f0d256e945fc6eb
Maxwell <145994855+ping-maxwell@users.noreply.github.com>
2026-02-26 17:37:32 -0800

    fix: incorrect svg attributes (#176)

--- apps/web/src/lib/themes/themes.tsx
+++ apps/web/src/lib/themes/themes.tsx
@@ -1138,7 +1138,7 @@ export const supabase: ThemeDefinition = {
 			<path
 				d="M63.7076 110.284C60.8481 113.885 55.0502 111.912 54.9813 107.314L53.9738 40.0627L99.1935 40.0627C107.384 40.0627 111.952 49.5228 106.859 55.9374L63.7076 110.284Z"
 				fill="url(#supabase-paint1_linear)"
-				fill-opacity="0.2"
+				fillOpacity="0.2"
 			/>
 			<path
 				d="M45.317 2.07103C48.1765 -1.53037 53.9745 0.442937 54.0434 5.041L54.4849 72.2922H9.83113C1.64038 72.2922 -2.92775 62.8321 2.1655 56.4175L45.317 2.07103Z"
@@ -1153,8 +1153,8 @@ export const supabase: ThemeDefinition = {
 					y2="71.8295"
 					gradientUnits="userSpaceOnUse"
 				>
-					<stop stop-color="#249361" />
-					<stop offset="1" stop-color="#3ECF8E" />
+					<stop stopColor="#249361" />
+					<stop offset="1" stopColor="#3ECF8E" />
 				</linearGradient>
 				<linearGradient
 					id="supabase-paint1_linear"
@@ -1165,7 +1165,7 @@ export const supabase: ThemeDefinition = {
 					gradientUnits="userSpaceOnUse"
 				>
 					<stop />
-					<stop offset="1" stop-opacity="0" />
+					<stop offset="1" stopOpacity="0" />
 				</linearGradient>
 			</defs>
 		</svg>
@@ -1348,12 +1348,12 @@ export const tailwind: ThemeDefinition = {
 			viewBox="0 0 54 33"
 			className="w-4"
 		>
-			<g clip-path="url(#tailwindcss-a)">
+			<g clipPath="url(#tailwindcss-a)">
 				<path
 					fill="#38bdf8"
-					fill-rule="evenodd"
+					fillRule="evenodd"
 					d="M27 0c-7.2 0-11.7 3.6-13.5 10.8 2.7-3.6 5.85-4.95 9.45-4.05 2.054.513 3.522 2.004 5.147 3.653C30.744 13.09 33.808 16.2 40.5 16.2c7.2 0 11.7-3.6 13.5-10.8-2.7 3.6-5.85 4.95-9.45 4.05-2.054-.513-3.522-2.004-5.147-3.653C36.756 3.11 33.692 0 27 0zM13.5 16.2C6.3 16.2 1.8 19.8 0 27c2.7-3.6 5.85-4.95 9.45-4.05 2.054.514 3.522 2.004 5.147 3.653C17.244 29.29 20.308 32.4 27 32.4c7.2 0 11.7-3.6 13.5-10.8-2.7 3.6-5.85 4.95-9.45 4.05-2.054-.513-3.522-2.004-5.147-3.653C23.256 19.31 20.192 16.2 13.5 16.2z"
-					clip-rule="evenodd"
+					clipRule="evenodd"
 				/>
 			</g>
 			<defs>
